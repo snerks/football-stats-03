e096e1e8ea7821386d838ab9b92cec91830d4193
snerks <martin.github@outlook.com>
2019-08-11 19:02:17 +0100

    Updates

---
+++ precache-manifest.e723a6e72d1a0089ece70f406ad90e64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf3babf88ceb42b012ae3b2de9bd89c",
+    "url": "/football-stats-03/index.html"
+  },
+  {
+    "revision": "dfa8cdffdb26243308dd",
+    "url": "/football-stats-03/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "0672d29eb072f8498708",
+    "url": "/football-stats-03/static/js/2.99572be3.chunk.js"
+  },
+  {
+    "revision": "dfa8cdffdb26243308dd",
+    "url": "/football-stats-03/static/js/main.3901f4ab.chunk.js"
+  },
+  {
+    "revision": "b84644f0046a4aad93a3",
+    "url": "/football-stats-03/static/js/runtime~main.8d9b25ef.js"
+  }
+]);
