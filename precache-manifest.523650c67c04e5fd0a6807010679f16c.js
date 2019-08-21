69b74d27c03b44fc0cdab7888c1315269feba0e5
snerks <martin.github@outlook.com>
2019-08-21 17:39:57 +0100

    Updates

--- precache-manifest.523650c67c04e5fd0a6807010679f16c.js
+++ precache-manifest.523650c67c04e5fd0a6807010679f16c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9e6dda1c4c7a13814cf36dda465ce830",
+    "revision": "b5f3a2babde431b0020c8f8da194d824",
     "url": "/football-stats-03/index.html"
   },
   {
-    "revision": "3a25c9497e5516c812ea",
+    "revision": "060040e911440524743e",
     "url": "/football-stats-03/static/css/main.19393e92.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/football-stats-03/static/js/2.99572be3.chunk.js"
   },
   {
-    "revision": "3a25c9497e5516c812ea",
-    "url": "/football-stats-03/static/js/main.42cc4d03.chunk.js"
+    "revision": "060040e911440524743e",
+    "url": "/football-stats-03/static/js/main.b56fe91b.chunk.js"
   },
   {
     "revision": "b84644f0046a4aad93a3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/football-stats-03/precache-manifest.4aa0199573ee609f5a9938989f8b06bc.js"
+  "/football-stats-03/precache-manifest.523650c67c04e5fd0a6807010679f16c.js"
 );
 
 self.addEventListener('message', (event) => {
