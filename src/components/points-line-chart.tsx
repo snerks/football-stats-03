7db00a89f6158e4f3bd94de179bb801ccfff75a3
snerks <martin.github@outlook.com>
2021-07-26 18:46:07 +0100

    Remove duplication in Line definitions

--- src/components/points-line-chart.tsx
+++ src/components/points-line-chart.tsx
@@ -10,15 +10,18 @@ import { points2020 } from "./points2020";
 import { points2021 } from "./points2021";
 interface PointsLineChartProps { }
 
+type ShowYears = {
+  [year: number]: boolean;
+}
+
+type PointsPerYear = {
+  [year: number]: number[];
+}
+
 interface PointsLineChartState {
   showTrendLines: boolean;
 
-  show2016: boolean;
-  show2017: boolean;
-  show2018: boolean;
-  show2019: boolean;
-  show2020: boolean;
-  show2021: boolean;
+  showYears: ShowYears;
 }
 
 class PointsLineChart extends React.Component<
@@ -28,12 +31,14 @@ class PointsLineChart extends React.Component<
   state = {
     showTrendLines: false,
 
-    show2016: false,
-    show2017: false,
-    show2018: false,
-    show2019: true,
-    show2020: true,
-    show2021: true
+    showYears: {
+      2016: false,
+      2017: false,
+      2018: false,
+      2019: true,
+      2020: true,
+      2021: true
+    }
   };
 
   handleShowTrendLines = () => {
@@ -45,55 +50,10 @@ class PointsLineChart extends React.Component<
     });
   };
 
-  handleShow2016 = () => {
+  handleShowYear = (year: number) => {
     this.setState(prevState => {
       const nextState = { ...prevState };
-      nextState.show2016 = !prevState.show2016;
-
-      return nextState;
-    });
-  };
-
-  handleShow2017 = () => {
-    this.setState(prevState => {
-      const nextState = { ...prevState };
-      nextState.show2017 = !prevState.show2017;
-
-      return nextState;
-    });
-  };
-
-  handleShow2018 = () => {
-    this.setState(prevState => {
-      const nextState = { ...prevState };
-      nextState.show2018 = !prevState.show2018;
-
-      return nextState;
-    });
-  };
-
-  handleShow2019 = () => {
-    this.setState(prevState => {
-      const nextState = { ...prevState };
-      nextState.show2019 = !prevState.show2019;
-
-      return nextState;
-    });
-  };
-
-  handleShow2020 = () => {
-    this.setState(prevState => {
-      const nextState = { ...prevState };
-      nextState.show2020 = !prevState.show2020;
-
-      return nextState;
-    });
-  };
-
-  handleShow2021 = () => {
-    this.setState(prevState => {
-      const nextState = { ...prevState };
-      nextState.show2021 = !prevState.show2021;
+      nextState.showYears[year] = !prevState.showYears[year];
 
       return nextState;
     });
@@ -130,75 +90,73 @@ class PointsLineChart extends React.Component<
     const orangeLineColour = `rgba(${orangeRgba}, 1)`;
     const orangeLegendFillColour = `rgba(${orangeRgba}, 0.4)`;
 
-    const pointsRunning2016: number[] = [];
+    const defaultChartDataSet: chartjs.ChartDataSets = {
+      // label: "2016-",
+      fill: false,
+      lineTension: 0.1,
 
-    for (let index = 0; index < points2016.length; index++) {
-      const runningTotal = index === 0 ? 0 : pointsRunning2016[index - 1];
+      // backgroundColor: aliceBlueLegendFillColour,
+      // borderColor: aliceBlueLineColour,
 
-      const nextElement = runningTotal + points2016[index];
+      borderCapStyle: "butt",
+      borderDash: [3, 3],
+      borderDashOffset: 0.0,
+      borderJoinStyle: "miter",
 
-      pointsRunning2016.push(nextElement);
+      // pointBorderColor: aliceBlueLineColour,
+
+      pointBackgroundColor: "#fff",
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+
+      // pointHoverBackgroundColor: aliceBlueLineColour,
+
+      pointHoverBorderColor: greyLineColour,
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      // data: pointsRunningTotalsPerYear[2016]
     }
 
-    const pointsRunning2017: number[] = [];
-
-    for (let index = 0; index < points2017.length; index++) {
-      const runningTotal = index === 0 ? 0 : pointsRunning2017[index - 1];
-
-      const nextElement = runningTotal + points2017[index];
-
-      pointsRunning2017.push(nextElement);
+    const pointsRunningTotalsPerYear: PointsPerYear = {
+      2016: [],
+      2017: [],
+      2018: [],
+      2019: [],
+      2020: [],
+      2021: [],
     }
 
-    const pointsRunning2018: number[] = [];
-
-    for (let index = 0; index < points2018.length; index++) {
-      const runningTotal = index === 0 ? 0 : pointsRunning2018[index - 1];
-
-      const nextElement = runningTotal + points2018[index];
-
-      pointsRunning2018.push(nextElement);
+    const pointsPerYear: PointsPerYear = {
+      2016: points2016,
+      2017: points2017,
+      2018: points2018,
+      2019: points2019,
+      2020: points2020,
+      2021: points2021,
     }
 
-    const pointsRunning2019: number[] = [];
+    for (const year in pointsRunningTotalsPerYear) {
+      if (Object.prototype.hasOwnProperty.call(pointsRunningTotalsPerYear, year)) {
+        const yearElement = pointsRunningTotalsPerYear[year];
 
-    for (let index = 0; index < points2019.length; index++) {
-      const runningTotal = index === 0 ? 0 : pointsRunning2019[index - 1];
+        for (let index = 0; index < pointsPerYear[year].length; index++) {
+          const runningTotal = index === 0 ? 0 : yearElement[index - 1];
 
-      const nextElement = runningTotal + points2019[index];
+          const nextElement = runningTotal + pointsPerYear[year][index];
 
-      pointsRunning2019.push(nextElement);
+          yearElement.push(nextElement);
+        }
+      }
     }
 
-    const pointsRunning2020: number[] = [];
-
-    for (let index = 0; index < points2020.length; index++) {
-      const runningTotal = index === 0 ? 0 : pointsRunning2020[index - 1];
-
-      const nextElement = runningTotal + points2020[index];
-
-      pointsRunning2020.push(nextElement);
-    }
-
-    const pointsRunning2021: number[] = [];
-
-    for (let index = 0; index < points2021.length; index++) {
-      const runningTotal = index === 0 ? 0 : pointsRunning2021[index - 1];
-
-      const nextElement = runningTotal + points2021[index];
-
-      pointsRunning2021.push(nextElement);
-    }
-
-    const dataPointCount = pointsRunning2018.length;
+    const dataPointCount = pointsPerYear[2016].length;
 
     const labels: string[] = [];
 
     for (let index = 0; index < dataPointCount; index++) {
-      // const gameNumber = index + 1;
       const gameNumber = index;
 
-      // const element = (index + 1) % 5 === 0 ? (index + 1).toString(10) : "";
       const element =
         (gameNumber % 5 === 0 ||
           gameNumber === 1 ||
@@ -212,199 +170,94 @@ class PointsLineChart extends React.Component<
     }
 
     const chartData: ChartData<chartjs.ChartData> = {
-      // labels: Array(dataPointCount)
-      //   .fill(0)
-      //   .map((e, i) => (i + 1).toString(10)),
-
       labels: labels,
 
       datasets: []
     };
 
-    if (this.state.show2016) {
+    if (this.state.showYears[2016]) {
       if (chartData.datasets) {
         chartData.datasets.push({
+          ...defaultChartDataSet,
           label: "2016-",
-          fill: false,
-          lineTension: 0.1,
-
           backgroundColor: aliceBlueLegendFillColour,
           borderColor: aliceBlueLineColour,
-
-          borderCapStyle: "butt",
-          borderDash: [3, 3],
-          borderDashOffset: 0.0,
-          borderJoinStyle: "miter",
           pointBorderColor: aliceBlueLineColour,
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
           pointHoverBackgroundColor: aliceBlueLineColour,
-          pointHoverBorderColor: greyLineColour,
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: pointsRunning2016
+          data: pointsRunningTotalsPerYear[2016],
         });
       }
     }
 
-    if (this.state.show2017) {
+    if (this.state.showYears[2017]) {
       if (chartData.datasets) {
         chartData.datasets.push({
+          ...defaultChartDataSet,
           label: "2017-",
-          fill: false,
-          lineTension: 0.1,
-
           backgroundColor: purpleLegendFillColour,
           borderColor: purpleLineColour,
-
-          borderCapStyle: "butt",
-          borderDash: [3, 3],
-          borderDashOffset: 0.0,
-          borderJoinStyle: "miter",
           pointBorderColor: purpleLineColour,
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
           pointHoverBackgroundColor: purpleLineColour,
-          pointHoverBorderColor: greyLineColour,
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: pointsRunning2017
+          data: pointsRunningTotalsPerYear[2017],
         });
       }
     }
 
-    if (this.state.show2018) {
+    if (this.state.showYears[2018]) {
       if (chartData.datasets) {
         chartData.datasets.push({
+          ...defaultChartDataSet,
           label: "2018-",
-          fill: false,
-          lineTension: 0.1,
           backgroundColor: slateBlueLegendFillColour,
           borderColor: slateBlueLineColour,
-          borderCapStyle: "butt",
-          borderDash: [3, 3],
-          borderDashOffset: 0.0,
-          borderJoinStyle: "miter",
           pointBorderColor: slateBlueLineColour,
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
           pointHoverBackgroundColor: slateBlueLineColour,
-          pointHoverBorderColor: greyLineColour,
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: pointsRunning2018
+          data: pointsRunningTotalsPerYear[2018],
         });
       }
     }
 
-    // if (this.state.show2019) {
-    //   if (chartData.datasets) {
-    //     chartData.datasets.push({
-    //       label: "2019-",
-    //       fill: false,
-    //       lineTension: 0.1,
-    //       backgroundColor: redLegendFillColour,
-    //       borderColor: redLineColour,
-    //       borderCapStyle: "butt",
-    //       borderDash: [],
-    //       borderDashOffset: 0.0,
-    //       borderJoinStyle: "miter",
-    //       pointBorderColor: redLineColour,
-    //       pointBackgroundColor: "#fff",
-    //       pointBorderWidth: 1,
-    //       pointHoverRadius: 5,
-    //       pointHoverBackgroundColor: redLineColour,
-    //       pointHoverBorderColor: greyLineColour,
-    //       pointHoverBorderWidth: 2,
-    //       pointRadius: 1,
-    //       pointHitRadius: 10,
-    //       data: pointsRunning2019
-    //     });
-    //   }
-    // }
-
-    if (this.state.show2019) {
+    if (this.state.showYears[2019]) {
       if (chartData.datasets) {
         chartData.datasets.push({
+          ...defaultChartDataSet,
           label: "2019-",
-          fill: false,
-          lineTension: 0.1,
           backgroundColor: slateBlueLegendFillColour,
           borderColor: slateBlueLineColour,
-          borderCapStyle: "butt",
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: "miter",
           pointBorderColor: slateBlueLineColour,
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
           pointHoverBackgroundColor: slateBlueLineColour,
-          pointHoverBorderColor: greyLineColour,
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: pointsRunning2019
+          borderDash: [],
+          data: pointsRunningTotalsPerYear[2019],
         });
       }
     }
 
-    if (this.state.show2020) {
+    if (this.state.showYears[2020]) {
       if (chartData.datasets) {
         chartData.datasets.push({
+          ...defaultChartDataSet,
           label: "2020-",
-          fill: false,
-          lineTension: 0.1,
-
           backgroundColor: purpleLegendFillColour,
           borderColor: purpleLineColour,
-
-          borderCapStyle: "butt",
-          borderDash: [3, 3],
-          borderDashOffset: 0.0,
-          borderJoinStyle: "miter",
           pointBorderColor: purpleLineColour,
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
           pointHoverBackgroundColor: purpleLineColour,
-          pointHoverBorderColor: greyLineColour,
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: pointsRunning2020
+          borderDash: [],
+          data: pointsRunningTotalsPerYear[2020],
         });
       }
     }
 
-    if (this.state.show2021) {
+    if (this.state.showYears[2021]) {
       if (chartData.datasets) {
         chartData.datasets.push({
+          ...defaultChartDataSet,
           label: "2021-",
-          fill: false,
-          lineTension: 0.1,
           backgroundColor: redLegendFillColour,
           borderColor: redLineColour,
-          borderCapStyle: "butt",
-          borderDash: [],
-          borderDashOffset: 0.0,
-          borderJoinStyle: "miter",
           pointBorderColor: redLineColour,
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
           pointHoverBackgroundColor: redLineColour,
-          pointHoverBorderColor: greyLineColour,
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: pointsRunning2021
+          borderDash: [],
+          data: pointsRunningTotalsPerYear[2021],
         });
       }
     }
@@ -427,53 +280,25 @@ class PointsLineChart extends React.Component<
 
       if (chartData.datasets) {
         chartData.datasets.push({
+          ...defaultChartDataSet,
           label: "Playoffs",
-          fill: false,
-          lineTension: 0.1,
-
           backgroundColor: greenLegendFillColour,
           borderColor: greenLineColour,
-
-          borderCapStyle: "butt",
-          borderDash: [5, 15],
-          borderDashOffset: 0.0,
-          borderJoinStyle: "miter",
           pointBorderColor: greenLineColour,
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
           pointHoverBackgroundColor: greenLineColour,
-          pointHoverBorderColor: greyLineColour,
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-
-          data: playoffFormValues
+          borderDash: [5, 15],
+          data: playoffFormValues,
         });
 
         chartData.datasets.push({
+          ...defaultChartDataSet,
           label: "Relegation",
-          fill: false,
-          lineTension: 0.1,
-
           backgroundColor: orangeLegendFillColour,
           borderColor: orangeLineColour,
-
-          borderCapStyle: "butt",
-          borderDash: [5, 15],
-          borderDashOffset: 0.0,
-          borderJoinStyle: "miter",
           pointBorderColor: orangeLineColour,
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
           pointHoverBackgroundColor: orangeLineColour,
-          pointHoverBorderColor: greyLineColour,
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-
-          data: relegationFormValues
+          borderDash: [5, 15],
+          data: relegationFormValues,
         });
       }
     }
@@ -520,75 +345,24 @@ class PointsLineChart extends React.Component<
           <label className="custom-control-label" htmlFor="customCheck1">
             Trend Lines
           </label>
-          <input
-            type="checkbox"
-            className="custom-control-input"
-            id="show2016Checkbox"
-            onChange={() => this.handleShow2016()}
-            style={{ marginLeft: "10px" }}
-            checked={this.state.show2016}
-          />
-          <label className="custom-control-label" htmlFor="show2016Checkbox">
-            2016-
-          </label>{" "}
-          <input
-            type="checkbox"
-            className="custom-control-input"
-            id="show2017Checkbox"
-            onChange={() => this.handleShow2017()}
-            style={{ marginLeft: "10px" }}
-            checked={this.state.show2017}
-          />
-          <label className="custom-control-label" htmlFor="show2017Checkbox">
-            2017-
-          </label>{" "}
-          <input
-            type="checkbox"
-            className="custom-control-input"
-            id="show2018Checkbox"
-            onChange={() => this.handleShow2018()}
-            style={{ marginLeft: "10px" }}
-            checked={this.state.show2018}
-          />
-          <label className="custom-control-label" htmlFor="show2018Checkbox">
-            2018-
-          </label>{" "}
-          <input
-            type="checkbox"
-            className="custom-control-input"
-            id="show2019Checkbox"
-            onChange={() => this.handleShow2019()}
-            style={{ marginLeft: "10px" }}
-            checked={this.state.show2019}
-          />
-          <label className="custom-control-label" htmlFor="show2019Checkbox">
-            2019-
-          </label>{" "}
 
-          <input
-            type="checkbox"
-            className="custom-control-input"
-            id="show2020Checkbox"
-            onChange={() => this.handleShow2020()}
-            style={{ marginLeft: "10px" }}
-            checked={this.state.show2020}
-          />
-          <label className="custom-control-label" htmlFor="show2020Checkbox">
-            2020-
-          </label>{" "}
-
-
-          <input
-            type="checkbox"
-            className="custom-control-input"
-            id="show2021Checkbox"
-            onChange={() => this.handleShow2021()}
-            style={{ marginLeft: "10px" }}
-            checked={this.state.show2021}
-          />
-          <label className="custom-control-label" htmlFor="show2021Checkbox">
-            2021-
-          </label>{" "}
+          {
+            Object.keys(pointsRunningTotalsPerYear).map(i =>
+            (!isNaN(+i) &&
+              <div key={+i} style={{ display: "inline" }}>
+                <input
+                  type="checkbox"
+                  className="custom-control-input"
+                  id={`show${i}Checkbox`}
+                  onChange={() => this.handleShowYear(+i)}
+                  style={{ marginLeft: "10px" }}
+                  checked={(this.state.showYears as any)[i]} />
+                <label className="custom-control-label" htmlFor={`show${i}Checkbox`}>
+                  {i}-
+                </label>
+              </div>
+            ))
+          }
         </div>
 
         <Line
